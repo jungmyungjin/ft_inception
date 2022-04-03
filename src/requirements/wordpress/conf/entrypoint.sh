811d24fd0d421f4caa58d86c58f6ad2e1e6fafb2
jungmyungjin <myung2667@gmail.com>
2022-04-03 21:25:55 +0900

    localhost를 login.42.fr 로 리다이렉션 되도록 수정

--- src/requirements/wordpress/conf/entrypoint.sh
+++ src/requirements/wordpress/conf/entrypoint.sh
@@ -4,6 +4,6 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
 wp config create --allow-root --dbname="wordpress" --dbuser="mjung" --dbpass="123456" --dbhost="mydb"
-wp core install --allow-root --url="localhost" --title="Blog Title" --admin_user="mjung" --admin_password="123456" --admin_email="mjung@student.42seoul.kr"
+wp core install --allow-root --url="mjung.42.fr" --title="Blog Title" --admin_user="mjung" --admin_password="123456" --admin_email="mjung@student.42seoul.kr"
 # chmod 777 -R ./*
 /usr/sbin/php-fpm7.3 -F -R
