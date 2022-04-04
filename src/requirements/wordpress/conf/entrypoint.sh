a61eb519665cbc0d7df594c4f6965d324c6a6502
jungmyungjin <myung2667@gmail.com>
2022-04-05 05:17:21 +0900

    wordpress 환경변수 사용하도록 수정

--- src/requirements/wordpress/conf/entrypoint.sh
+++ src/requirements/wordpress/conf/entrypoint.sh
@@ -2,8 +2,8 @@
 
 cd /share_wordpress/wordpress
 if [ ! -e /wordpress_workdir/wordpress/wp-config.php ]; then
-	wp config create --allow-root --dbname="wordpress" --dbuser="mjung" --dbpass="123456" --dbhost=${DB_IP}
-	wp core install --allow-root --url="mjung.42.fr" --title="Admin Blog" --admin_user="mjung" --admin_password="123456" --admin_email="mjung@student.42seoul.kr"
-	wp user create user user@student.42seoul.kr --allow-root --user_pass=1234
+	wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${DB_IP}
+	wp core install --allow-root --url=${DOMAIN_NAME} --title="${BLOG_TITLE}" --admin_user=${ADMIN_USER} --admin_password=${ADMIN_PASSWD} --admin_email=${ADMIN_EMAIL}
+	wp user create ${GENERAL_USER} ${GENERAL_EMAIL} --allow-root --user_pass=${GENERAL_PASSWD}
 fi
 /usr/sbin/php-fpm7.3 -F -R
