17fe70f84f8254798430b1c5418418657c0b1866
jungmyungjin <myung2667@gmail.com>
2022-04-04 22:27:28 +0900

    wordpress 설치 개선

--- src/requirements/wordpress/conf/entrypoint.sh
+++ src/requirements/wordpress/conf/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-wp core download --allow-root
-wp config create --allow-root --dbname="wordpress" --dbuser="mjung" --dbpass="123456" --dbhost="mydb"
-wp core install --allow-root --url="mjung.42.fr" --title="Blog Title" --admin_user="mjung" --admin_password="123456" --admin_email="mjung@student.42seoul.kr"
-# chmod 777 -R ./*
+cd /share_wordpress/wordpress
+if [ ! -e /wordpress_workdir/wordpress/wp-config.php ]; then
+	wp config create --allow-root --dbname="wordpress" --dbuser="mjung" --dbpass="123456" --dbhost=${DB_IP}
+	wp core install --allow-root --url="mjung.42.fr" --title="Admin Blog" --admin_user="mjung" --admin_password="123456" --admin_email="mjung@student.42seoul.kr"
+	wp user create user user@student.42seoul.kr --allow-root --user_pass=1234
+fi
 /usr/sbin/php-fpm7.3 -F -R
