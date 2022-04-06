07909e592d16c43f81bcc02c4598a5098067348f
jungmyungjin <myung2667@gmail.com>
2022-04-07 08:05:43 +0900

    service 데몬 실행 PID가 1로 오도록 수정

--- src/requirements/mariadb/tools/entrypoint.sh
+++ src/requirements/mariadb/tools/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+set -e
 
-TEMPLATE_SCRIPT_PATH='/mariaDB/script.sql.template'
-SCRIPT_PATH='/mariaDB/script.sql'
+echo "/$\0 = $0, /$\1 = $1"
 
-if [ ! -f "$SCRIPT_PATH" ]; then
-    echo "$TEMPLATE_SCRIPT_PATH exists."
-		cat $TEMPLATE_SCRIPT_PATH | envsubst > $SCRIPT_PATH;
-		# mysqld_safe --init-file $SCRIPT_PATH
-		mysqld_safe --init-file $SCRIPT_PATH
-
-else
-		# mysqld_safe
-		mysqld_safe
+if [ "$1" = 'mysqld_safe' ]; then
+	TEMPLATE_SCRIPT_PATH='/mariaDB/script.sql.template'
+	SCRIPT_PATH='/mariaDB/script.sql'
+	echo "IN /$\0 = $0, /$\1 = $1"
+	if [ ! -f "$SCRIPT_PATH" ]; then
+	    echo "$TEMPLATE_SCRIPT_PATH exists."
+			cat $TEMPLATE_SCRIPT_PATH | envsubst > $SCRIPT_PATH;
+			exec mysqld_safe --init-file $SCRIPT_PATH
+	fi
 fi
+exec "$@"
--- src/requirements/nginx/tools/entrypoint.sh
+++ src/requirements/nginx/tools/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -e
 
 TEMPLATE_SCRIPT_PATH='/nginx_workdir/defualt.conf.template'
 HOME_COPY='/nginx_workdir/defualt.conf'
 SCRIPT_PATH='/etc/nginx/sites-available/default'
 
-if [ ! -f "$HOME_COPY" ]; then
-    echo "$HOME_COPY (home copy) exists."
-		cat $TEMPLATE_SCRIPT_PATH | envsubst '${DOMAIN_NAME}'> $HOME_COPY;
-		cp $HOME_COPY $SCRIPT_PATH
+if [ "$1" = 'nginx' ]; then
+	if [ ! -f "$HOME_COPY" ]; then
+			cat $TEMPLATE_SCRIPT_PATH | envsubst '${DOMAIN_NAME}'> $HOME_COPY;
+			cp $HOME_COPY $SCRIPT_PATH
+	fi
 fi
-
-nginx -g "daemon off;"
+exec "$@"
