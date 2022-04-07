2fda62c241f2eced49169c24ad6b04acfc7fd998
jungmyungjin <myung2667@gmail.com>
2022-04-07 09:04:14 +0900

    init.sh 스크립트 수정

--- tools/init.sh
+++ tools/init.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 USER_NAME="mjung"
-# DOCKER_MOUNT_PATH="/home/$USER_NAME/data"
-
-# test mac
-# DOCKER_MOUNT_PATH="~/data"
-
-# if [ ! -e $DOCKER_MOUNT_PATH ]; then
-if [ ! -e ~/data ]; then
-    # mkdir -p $DOCKER_MOUNT_PATH/web
-    # mkdir -p $DOCKER_MOUNT_PATH/db
     mkdir -p ~/data/web
     mkdir -p ~/data/db
-fi
 
 
