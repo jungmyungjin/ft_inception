937b149ff4b206a5552eae355dd8a6e59ea4889d
jungmyungjin <myung2667@gmail.com>
2022-04-07 09:00:02 +0900

    불필요 주석 제거

--- src/requirements/mariadb/tools/entrypoint.sh
+++ src/requirements/mariadb/tools/entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "/$\0 = $0, /$\1 = $1"
 
 if [ "$1" = 'mysqld_safe' ]; then
 	TEMPLATE_SCRIPT_PATH='/mariaDB/script.sql.template'
 	SCRIPT_PATH='/mariaDB/script.sql'
-	echo "IN /$\0 = $0, /$\1 = $1"
 	if [ ! -f "$SCRIPT_PATH" ]; then
-	    echo "$TEMPLATE_SCRIPT_PATH exists."
 			cat $TEMPLATE_SCRIPT_PATH | envsubst > $SCRIPT_PATH;
 			exec mysqld_safe --init-file $SCRIPT_PATH
 	fi
